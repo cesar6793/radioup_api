6979401423a8debb4118758c93161dae250aecfc
cesar <cesar6793@gmail.com>
2024-06-21 21:50:51 -0500

    m

--- app/main.py
+++ app/main.py
@@ -26,10 +26,6 @@ app.mount("/files", StaticFiles(directory="files"), name="files")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     os.makedirs("files", exist_ok=True)
